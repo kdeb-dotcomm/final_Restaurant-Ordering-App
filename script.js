3363a7d34a811295822da2cec0fc1787c1673e1b
kdeb-dotcomm <eugenedebrah07@gmail.com>
2025-08-22 16:58:40 +0000

    pushing again

--- script.js
+++ script.js
@@ -1,73 +1,184 @@
 import { menuArray } from './data.js';
 
 const containerParent = document.querySelector('.big-container');
+let orderContainer = ''; // to hold the order container
+let totalPrice = 0; // to hold the total price of the order
+let totalPriceValue = null; 
+let orderItemsContainer = null;
+
+function addToTotal(price) {
+    totalPrice += Number(price);
+    if (totalPriceValue) totalPriceValue.textContent = `$${totalPrice.toFixed(2)}`;
+}
 
 
 
-function render(){
+function render() {
     containerParent.innerHTML = '';
     const menuBtn = document.createElement('h1');
     menuBtn.classList.add('menu');
     menuBtn.textContent = 'Menu';
     containerParent.appendChild(menuBtn);
+    // listening to the click event on the menu button
+    menuBtn.addEventListener('click', () => {
+        // removing the menu button
+        containerParent.removeChild(menuBtn);
+        // rendering the menu items
+        renderMenuItems();
+    });
 
-// rendering the menu items
-menuArray.map((item) => {
-    // creating the item container
-    const itemContainer = document.createElement('div');
-    itemContainer.classList.add('category-container');
+}
 
-    // creating the item elements
-    // handling the image
-    const itemImage = document.createElement('p');
-    itemImage.classList.add('category-img');
-    itemImage.textContent = item.emoji;
-    itemContainer.appendChild(itemImage);
+function renderMenuItems() {
+    // rendering the menu items
+    menuArray.map((item) => {
+        // creating the item container
+        const itemContainer = document.createElement('div');
+        itemContainer.classList.add('category-container');
 
-    // let create a div for the text content
-    // handling the title and description
+        // creating the item elements
+        // handling the image
+        const itemImage = document.createElement('p');
+        itemImage.classList.add('category-img');
+        itemImage.textContent = item.emoji;
+        itemContainer.appendChild(itemImage);
 
-
-    const textContainer = document.createElement('div');
-    textContainer.classList.add('category-desc');
-    itemContainer.appendChild(textContainer);
-    // handling the title
-    const itemTitle = document.createElement('h2');
-    itemTitle.classList.add('category-title');
-    itemTitle.textContent = item.name;
-    textContainer.appendChild(itemTitle);
-
-    // handling the description
-    const itemDescription = document.createElement('p');
-    itemDescription.classList.add('category-description');
-    itemDescription.textContent = item.ingredients;
-    textContainer.appendChild(itemDescription);
-
-    // handling the price
-    const itemPrice = document.createElement('span');
-    itemPrice.classList.add('menu-price');
-    itemPrice.textContent = `$${item.price}`;
-    textContainer.appendChild(itemPrice);
-
-    // handling the plus btn
-    // handling the plus btn
-const plusContainer = document.createElement('div');
-plusContainer.classList.add('category-plus-container');
-
-const plusBtn = document.createElement('button');
-plusBtn.classList.add('category-plus');
-plusBtn.textContent = '+';
-
-// put button inside its container
-plusContainer.appendChild(plusBtn);
+        // let create a div for the text content
+        // handling the title and description
 
 
-itemContainer.appendChild(plusContainer);
+        const textContainer = document.createElement('div');
+        textContainer.classList.add('category-desc');
+        itemContainer.appendChild(textContainer);
+        // handling the title
+        const itemTitle = document.createElement('h2');
+        itemTitle.classList.add('category-title');
+        itemTitle.textContent = item.name;
+        textContainer.appendChild(itemTitle);
 
-    
-    containerParent.appendChild(itemContainer);
-    
-})
+        // handling the description
+        const itemDescription = document.createElement('p');
+        itemDescription.classList.add('category-description');
+        itemDescription.textContent = item.ingredients;
+        textContainer.appendChild(itemDescription);
+
+        // handling the price
+        const itemPrice = document.createElement('span');
+        itemPrice.classList.add('menu-price');
+        itemPrice.textContent = `$${item.price}`;
+        textContainer.appendChild(itemPrice);
+
+        // handling the plus btn
+        const plusContainer = document.createElement('div');
+        plusContainer.classList.add('category-plus-container');
+
+        const plusBtn = document.createElement('button');
+        plusBtn.classList.add('category-plus');
+        plusBtn.textContent = '+';
+
+        // put button inside its container
+        plusContainer.appendChild(plusBtn);
+        // adding an event listener to the plus button
+        plusBtn.addEventListener('click', () => {
+            //creating the order container
+            // let create our order container once
+            if (!orderContainer) {
+                orderContainer = document.createElement('div');
+                orderContainer.classList.add('order-container');
+                // appending the order container to the body
+                document.body.appendChild(orderContainer);
+                // creating the order title
+                const orderTitle = document.createElement('h2');
+                orderTitle.classList.add('order-title');
+                orderTitle.textContent = 'Your Order';
+                orderContainer.appendChild(orderTitle);
+
+                // creating a container for my line
+                orderItemsContainer = document.createElement('div');
+                orderItemsContainer.classList.add('order-items-container');
+                orderContainer.appendChild(orderItemsContainer);
+
+                // now let get a line to separate the items from the total price
+                const separator = document.createElement('hr');
+                separator.classList.add('order-separator');
+                orderContainer.appendChild(separator);
+
+                // creating the total price container
+                const totalPriceContainer = document.createElement('div');
+                totalPriceContainer.classList.add('total-price-container');
+                orderContainer.appendChild(totalPriceContainer);
+
+                // creating the total price text
+                const totalPriceText = document.createElement('span');
+                totalPriceText.classList.add('total-price-text');
+                totalPriceText.textContent = 'Total Price: ';
+                totalPriceContainer.appendChild(totalPriceText); 
+
+                // creating the total price value
+                totalPriceValue = document.createElement('span');
+                totalPriceValue.classList.add('total-price-value');
+                totalPriceValue.textContent = '$0.00'; 
+                totalPriceContainer.appendChild(totalPriceValue);
+
+                
+
+            }
+
+            // selecting the line
+            orderItemsContainer = orderContainer.querySelector('.order-items-container');            
+            // creating the individual order container
+            const orderItem = document.createElement('div');
+            orderItem.classList.add('order-item');
+            orderItemsContainer.appendChild(orderItem);
+
+            // creating the order item elements
+            const orderItemText = document.createElement('h2');
+            orderItemText.classList.add('order-item-text');
+            orderItemText.textContent = item.name;
+            orderItem.appendChild(orderItemText);
+
+
+            // creating the remove button
+            const removeBtn = document.createElement('button');
+            removeBtn.classList.add('remove-btn');
+            removeBtn.textContent = 'Remove';
+            orderItem.appendChild(removeBtn);
+
+            // adding event listener to our remove btn
+            removeBtn.addEventListener('click', () => {
+                orderItem.remove(); 
+                totalPrice -= Number(item.price);
+                totalPriceValue.textContent = `$${totalPrice.toFixed(2)}`; 
+            })
+
+            // creating the order item price
+            // let put the price inside a div to be safe
+            const orderItemPriceContainer = document.createElement('div');
+            orderItemPriceContainer.classList.add('order-item-price-container');
+            orderItem.appendChild(orderItemPriceContainer);
+
+
+            const orderItemPrice = document.createElement('span');
+            orderItemPrice.classList.add('order-item-price');
+            orderItemPrice.textContent = `$${item.price}`;
+            orderItemPriceContainer.appendChild(orderItemPrice);
+
+            
+
+            addToTotal(item.price); // updating the total price
+
+
+
+
+        });
+
+
+        itemContainer.appendChild(plusContainer);
+
+
+        containerParent.appendChild(itemContainer);
+
+    })
 
 }
 // rendering the menu btn and content
